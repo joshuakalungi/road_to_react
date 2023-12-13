85ea9af0f162f9e131b437df59f1329bd0bdc25c
joshuakalungi <joshuakalungi101@gmail.com>
2023-12-13 13:03:27 +0300

    HTML objects in JSX

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -2,7 +2,15 @@ import * as React from "react";
 
 
 // variable declaration ------outside the component
-const title = "React";
+// javascript string primitive
+// const title = "React";
+
+// javascript object 
+const welcome = {
+  greeting: "Hey",
+  title: "React"
+
+}
 
 
 function App(){
@@ -11,7 +19,7 @@ function App(){
 
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>{welcome.greeting} {welcome.title}</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
