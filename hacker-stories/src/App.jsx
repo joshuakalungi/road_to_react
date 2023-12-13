6c012723964343eed645d0b4d5364b7ccc525370
joshuakalungi <joshuakalungi101@gmail.com>
2023-12-13 12:51:53 +0300

    HTML attributes

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -12,6 +12,10 @@ function App(){
   return (
     <div>
       <h1>Hello {title}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+
     </div>
   );
 }
